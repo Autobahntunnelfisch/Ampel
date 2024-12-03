b012ef8a0aa73d2e10e336c7abf38d5478338858
Autobahntunnelfisch <cankinik@web.de>
2024-12-03 13:50:26 +0100

    add traffic lights for human and add print

--- ampel.dart
+++ ampel.dart
@@ -1,15 +1,64 @@
+
+import 'dart:io';
 void main() {
+  String actualSituation = "Straße2grün";
+  String actualHumansLight;
 
-  String color = "grün";
+  while(true) {    
+    switch (actualSituation) {
 
-  switch (color) {
+      case "Straße1grün":
 
-    case "red":
-      print("Stehen");
-    case "gelb":
-      print("Bereit machen");
-    case "grün":
-      print("Fahren");
+        print("Straße 1 grün | Straße 2 rot | Fußgänger 1 rot | Fußgänger 2 grün");
+        print("");
+        print("""
+                      |        |
+                      |        |
+                      |  STOP  | 
+          ----------------------------------
+           <<<<<<<      <<<<<<<      <<<<<<<
+           -     -     -   GO  -     -     -
+           >>>>>>>      >>>>>>>      >>>>>>>
+          ----------------------------------
+                      |  STOP  |
+                      |        |
+                      |        |
+                    """);
+        sleep(Duration(seconds:5));
+        actualSituation = "Straße2grün";
+        actualHumansLight = "Übergang1grün";
+        break;
+
+      case "Straße2grün":
+        print("Straße 2 grün | Straße 1 rot | Fußgänger 1 grün | Fußgänger 2 rot");
+        print("");
+        print("""
+                    |  V  |  ^  |
+                    |  V     ^  |
+                    |  V  |  ^  | 
+          ----------|           |-------------           
+                   S|     |     |S
+          -     -  T|     GO    |T    -     -
+                   O|     |     |O
+                   P|           |P
+          ----------|     |     |-------------
+                    |  V     ^  | 
+                    |  V  |  ^  |
+                    |  V     ^  |
+                    """);
+        sleep(Duration(seconds:5));
+        actualSituation = "Straße1grün";
+        actualHumansLight = "Übergang2grün";
+        break;
+
+      default:
+      print("Unknown situation");
+      return;
   }
+  
+  
+  }
+
+
 
 }
