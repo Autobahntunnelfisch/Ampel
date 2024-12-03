b88a7ed54d9798ab90876b76e341551a08c74710
Autobahntunnelfisch <cankinik@web.de>
2024-12-03 12:11:14 +0100

    Initial commit

---
+++ ampel.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  String color = "grün";
+
+  switch (color) {
+
+    case "red":
+      print("Stehen");
+    case "gelb":
+      print("Bereit machen");
+    case "grün":
+      print("Fahren");
+  }
+
+}
